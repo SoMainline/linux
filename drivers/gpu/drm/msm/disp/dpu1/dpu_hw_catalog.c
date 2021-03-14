78ee63b00970118a077cfcc6fbd9c2d05bde7986
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2021-03-14 04:58:32 +0300

    drm/msm/dpu1: use one active CTL if it is available
    Unlike previous generation, with newer ("active") CTLs it is possible to
    use just one CTL to handle both interfaces. And one has to use single
    CTL to support master/slave DSI config. So use one active CTL if it is
    available.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.c
@@ -445,6 +445,7 @@ static const struct dpu_caps sm8150_dpu_caps = {
 	.has_dim_layer = true,
 	.has_idle_pc = true,
 	.has_3d_merge = true,
+	.has_active_ctls = true,
 	.max_linewidth = 4096,
 	.pixel_ram_size = DEFAULT_PIXEL_RAM_SIZE,
 	.max_hdeci_exp = MAX_HORZ_DECIMATION,
@@ -491,6 +492,7 @@ static const struct dpu_caps sm8250_dpu_caps = {
 	.has_dim_layer = true,
 	.has_idle_pc = true,
 	.has_3d_merge = true,
+	.has_active_ctls = true,
 	.max_linewidth = 4096,
 	.pixel_ram_size = DEFAULT_PIXEL_RAM_SIZE,
 };
@@ -506,6 +508,7 @@ static const struct dpu_caps sm8350_dpu_caps = {
 	.has_idle_pc = true,
 	.has_3d_merge = true,
 	.max_linewidth = 4096,
+	.has_active_ctls = true,
 	.pixel_ram_size = DEFAULT_PIXEL_RAM_SIZE,
 };
 
