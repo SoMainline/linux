ca800a3c2df2db908b79fb3a9e08af63054cd43f
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-08-04 16:05:16 +0200

    clk_ignore_unused

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -1432,7 +1432,7 @@ static int __init clk_disable_unused(void)
 {
 	struct clk_core *core;
 
-	if (clk_ignore_unused) {
+	if (clk_ignore_unused || 1) {
 		pr_warn("clk: Not disabling unused clocks\n");
 		return 0;
 	}
