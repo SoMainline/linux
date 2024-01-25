2fe5ab647a14ddd5225fed81fc71bf0b8710e5a4
Konrad Dybcio <konrad.dybcio@linaro.org>
2024-01-25 20:53:33 +0100

    what is this even

--- drivers/power/supply/x13s_ec.c
+++ drivers/power/supply/x13s_ec.c
@@ -13,39 +13,128 @@
 #include <linux/pm_runtime.h>
 #include <linux/gpio/consumer.h>
 
+/* I2C commands */
 #define REG_SMBUS_MULTI_READ		0x00
 #define REG_SMBUS_MULTI_WRITE		0x01
 #define REG_SMBUS_READ			0x02
 #define REG_SMBUS_WRITE			0x03
 
-#define REG_REV_MAJ			0x00
+/* SMBUS_R/W registers */
+#define REG_01				0x01
+//bit(0) is settable
 
-#define REG_REV_MIN			0x01 // ?
+#define REG_02				0x02
+//BIT(1) by default, 0 setable
 
-#define REG_SCMS_15			0x15
- #define REG_SCMS_15_FN_STICKY			BIT(0) // write val of 0x51[4] here
+#define REG_03				0x03
+ //BIT(2) settable?
+
+#define REG_05				0x05
+ //0x0 vs 0x18
+
+#define REG_08				0x08
+ // 0xb3(def) vs 0x1e
+
+#define REG_LED_STS			0x0a
+ #define REG_LED_STS_FNLOCK		BIT(4)
+
+#define REG_STH				0x0b
+ #define REG_STH_4			BIT(4)
+ #define REG_STH_PLUGGEDIN		BIT(6)
+
+#define REG_LED_STS2			0x0c // R/O
+ #define REG_LED_STS2_AUDIO_MUTE	BIT(0)
+ #define REG_LED_STS2_CAPSLOCK		BIT(2)
+ #define REG_LED_STS2_X			BIT(4)
+
+#define REG_0F				0x0F
+ //0x30(def) vs 0x1
+
+#define REG_10				0x10
+ //0x80, 0xa0
+
+#define REG_11				0x11
+  //0x0 vs 0xb1
+
+#define REG_12				0x12
+  //0x7 vs 0x57
+
+#define REG_SCMS_15			0x15 // fn stupid mode?
+ #define REG_SCMS_15_FN_STICKY			BIT(0) // write val of 0x51[4] here?
  #define REG_SCMS_15_VAL		GENMASK(2, 1) // TODO: QEVs 0x73, 0x70, 0x72 fire on press
- // one of the modes: juggle between fn normal, for 1 press and sticky
+  #define REG_SCMS_15_VAL_XXX 2 //also \/ ???
+  #define REG_SCMS_15_VAL_CYCLE	1 // one of the modes: juggle between fn normal, for 1 press and sticky
 
-#define REG_51				0x51
- #define REG_51_FN_STICKY_STATE		BIT(0) // wr here to turn off
- #define REG_51_FN_STICKY		BIT(4)
+/* 0x16..0x17,  0x19..0x3c - some 1-bit r/w regs*/
+
+#define REG_4A				0x4A
+// 0x0default - 0x0f
+
+#define REG_53_0			0x50 // 0x1 vs vs 0x04(def) vs 0x24 (bmsk?)
+
+#define REG_FNLOCK			0x51
+ #define REG_FNLOCK_DISABLED		BIT(0) // r(status)/w(disable only)
+ //maybe persistent setting storage??
+ #define REG_FNLOCK_EN			BIT(4) // write => next keypress has fn (under what circumstances?)
+ #define REG_FNLOCK_UNK			BIT(7)
+
+#define REG_52				0x52
+ // 0x84 (def) vs 0x5
 
 #define REG_53				0x53
- #define REG_53_0			BIT(0)
- #define REG_SOME_KEY_TRISTATE		GENMASK(3, 2)
-  #define SOME_KEY_OFF			0
-  #define SOME_KEY_L1			1 // unused?
-  #define SOME_KEY_L2			2
-  /* 3 - res / ignore */
- #define REG_AUDIO_MUTE_KEY		BIT(4)
+ #define REG_53_FNKEY_TRISTATE		GENMASK(1, 0)
+  #define SOME_KEY_OFF			0 /* Fn is not active */
+  #define SOME_KEY_ONESHOT		1 /* Tap Fn once, then press anoher key */
+  #define SOME_KEY_PERMA		2 /* Double-tap Fn for FnLock (with no LED) */
+  /* 3 - unused */
+ #define REG_53_FNKEY_TRISTATE		GENMASK(3, 2)
+  /* 0 - res / ignore */
+  #define SOME_KEY_L0			1
+  #define SOME_KEY_L1			2
+  #define SOME_KEY_L2			3
+ #define REG_AUDIO_MUTE_LED		BIT(4) // set/clr on/off/get
+
+/* 0x54..0x55 - 1-bit */
+
+#define REG_57				0x57
+ // 0x40(def) vs 0x1
+
+#define REG_5B				0x5B
+ // 0x18 (def) vs 0x0
+
+/* 0x60..0x6f - 1bit regs */
+
+#define REG_70				0x70
+ #define REG_70_LID_OPEN		BIT(1) //set if open
+ #define REG_70_PLUGGEDIN		BIT(5)
+
+// 0x71 - 1bit
 
 #define REG_73				0x73
- 
 
-#define REG_7C				0x7C
+#define REG_74				0x74
+// Reg 0x74 changed from 0x1 to 0x8
 
-#define REG_7D				0x7D
+#define REG_75				0x75
+ #define REG_75_0			BIT(0)
+ #define REG_75_2			BIT(2)
+ #define REG_75_LCD_BL_OFF		BIT(5)
+ #define REG_75_6			BIT(6)
+
+#define REG_77				0x77
+ #define REG_77_FN_HELD_DOWN		BIT(2) // ???
+ #define REG_77_4			BIT(4) //bryan has it on
+ #define REG_77_LCD_BL_ON		BIT(5)
+
+#define REG_7A				0x7A
+//0x1 -> 0x10
+
+/* these two consume QEVs */
+#define REG_QEV_X			0x7C
+#define REG_QEV_Y			0x7D
+
+#define REG_STH2			0x7f
+ #define REG_STH2_PLUGGEDIN		BIT(6)
 
 #define REG_SUS_CTL			0x80
  #define REG_SUS_CTL_SUS_ENTER		0x55
@@ -53,41 +142,136 @@
  #define REG_SUS_CTL_SUS_EXIT		0xAA
  #define REG_SUS_CTL_S0IX_EXIT		0xBB //also called on disp on notif
 
+#define REG_81				0x81
+// 0 def vs 0x12
+
+#define REG_82				0x82
+// 0xa def vs 0x1e
+
+#define REG_84				0x84
+// 0x2(def) vs 0x3
+
+#define REG_8A				0x8A
+ #define REG_8A_MAYBE_BATDRAINING	BIT(0)
+
+#define REG_8B				0x8B
+ //keeps pulsin between 0x1 and 0x7 on readback
+
+#define REG_8C				0x8C
+ //bit1
+
+#define REG_8D				0x8D
+ //bit1
+
+#define REG_8E				0x8E
+ //bit1
+
+#define REG_8F				0x8F
+ //bit1
+
+// 0x91 1bit? */
+
+#define REG_94				0x94
+ // REG 0x94 changed from 0x1 to 0xff
+
+#define REG_97				0x97
+//bit0
+
 #define REG_A8				0xA8
- #define A8_0				BIT(0)
+ #define A8_0				BIT(0) //keeps pulsing randomly
 
 #define REG_A9				0xA9
- #define A9_X				GENMASK(4, 0)
- #define A9_5				BIT(5)
+ #define A9_X				GENMASK(4, 0) //bit 0 pulses randomly
+ #define A9_5				BIT(5) // stays on
+
+#define REG_AA				0xAA
+ //bti 0 keeps pulsing
+ // 0x22-0x23
+
+#define REG_AB				0xAB
+ //bits (012) and 3 pulse randomly
+ //bit 5 stays on
+
+#define REG_AC				0xAC
+ #define REG_AC_0			BIT(0) //keeps pulsing randomly
+ #define REG_AC_1			BIT(1)
+ #define REG_AC_2			BIT(2)
+ #define REG_AC_5			BIT(5)
+
+#define REG_AD				0xAD
+ //see reg_ab
+
+#define REG_BD				0xbd
+ //0x0 (def) vs 0x1
 
 #define REG_KBD				0xC0
- #define REG_KBD_MICMUTE		BIT(0)
- // BIT(1) is also micmute!?
+ #define REG_KBD_MICMUTE		BIT(0) // wr here to turn on
+ #define REG_KBD_SCMS_0X20		GENMASK(2, 1) // SCMS() gets this value and writes to 0x15[2:1]/0xc0[5:4]
+  #define SOMETHING_X			0
+  #define SOMETHING_Y			1
+  #define SOMETHING_Z			2
  #define REG_KBD_BKL_LVL		GENMASK(5, 4) //brightness only
   #define KBD_BKL_LVL_OFF		0
   #define KBD_BKL_LVL_LO		1
   #define KBD_BKL_LVL_HI		2
+ #define REG_KBD_6			BIT(6) //unk, on at boot
   /* 3 - unknown/reserved? */
 
+#define REG_C3				0xC3
+ //0x1 (def)vs 0x0?
+
 #define REG_C5				0xC5
- #define REG_C5_0			BIT(0)
- #define REG_C5_7			BIT(7)
+ #define REG_C5_0			BIT(0) // INGO(0x03) & 0xCF, if ~BIT(0), set it, unset otherwise
+ #define REG_C5_7			BIT(7) // INGO(0x03, 0x1) & ~0xCF[7], set it, if INGO(0x03, 0x1) & 0xCF[7] clr it
 
 #define REG_C6				0xC6 //convertible button
  #define REG_C6_CONERTIBLE_STATE	GENMASK(3, 0)
 
+#define REG_C9				0xC9
+ // 0x4(def) vs 0x1
+
 #define REG_CA				0xCA
  #define CA_0				BIT(0) // ????
- #define REG_CA_WDOG_STH		BIT(1) // if true DYTC(0x001f1001) else DYTC(0x000f1001)
+ #define REG_CA_SUS_ALLOWED		BIT(1) // leds blink if true, if true DYTC(0x001f1001) else DYTC(0x000f1001)
  #define REG_EN_KBD_CTL			BIT(2) // some enable
  #define REG_EN_KB_BKL			BIT(3) // some optional value to be used after setting BIT(2)
+ #define REG_CA_5			BIT(5) // on by default
  #define REG_EN_SOMETHING_WAKEUP	BIT(6)
  #define REG_CA_KBD_LIGHT_PRESENT_MAYBE	BIT(7)
 
+#define REG_CD				0xCD
+ //bit 0|1 -> bit0
+
+#define REG_CE				0xCE
+ //0x0 (def) vs 0x1
 
 #define REG_CF				0xCF
- #define REG_CF_0			BIT(0)
+ #define REG_CF_0			BIT(0) // INGO(=0x04, 0x1), if BIT(0), flip it
+ #define REG_CF_2			BIT(2)
+ #define REG_CF_5			BIT(5)
+ #define REG_CF_7			BIT(7) // INGO(=0x03, 0x0), if ~BIT(7), set it, unset otherwise
 
+#define REG_E1				0xE1
+ //bit0
+
+#define REG_F6				0xF6
+// 0x37(def) vs 0x39
+
+#define REG_FC				0xFC
+  //0x31(def) vs 0x33
+
+#define REG_FF				0xFF
+ //bit0, may be correlated w/ 0x22
+
+// polling period: 10 ms
+// passive <=95C, critical 99C
+/* SMBUS_MULTI_R/W regs */
+#define REG_TEMP0_VAL			0x2020 // TZ70, deci C
+#define NUM_THERMAL_SENSORS		6
+
+/* Simple readback regs */
+#define REG_REV_MAJ			0x00
+#define REG_REV_MIN			0x01 // ?
 #define REG_QUICK_EVENT			0xf0
 enum ec_quick_events {
 	EC_QEV_SLEEP = 0x13, /* Fn + 4 */
@@ -96,7 +280,7 @@ enum ec_quick_events {
 	EC_QEV_MICMUTE = 0x28, /* Fn + F4 */ // won't work due to 0x8 == HPDD & 0x08 == 0x08?
 	EC_QEV_AIRPLANE = 0x2a, /* Fn + F8 */
 	EC_QEV_THERMAL_ALERT = 0x36, //TZ70, acpi sets it to 127deg briefly
-	EC_QEV_WDOG_MAYBE = 0x3c, /* Pops up every 90s or more, wdog/heartbeat? */
+	EC_QEV_PERF_MODE_MAYBE = 0x3c, /* Maybe some LPM/idle entry posible notif? */
 	EC_QEV_CHG_CONNECT = 0x50, /* only works when power actually flows, LED blinks 3 times */
 	EC_QEV_CHG_DISCONECT = 0x51,
 	EC_QEV_LID_OPEN = 0x52,
@@ -112,8 +296,6 @@ enum ec_quick_events {
 	EC_QEV_CONVERTIBLE_BTN = 0x77,
 };
 
-#define REG_TEMP_DECICELCIUS		0x2020 /* ECMR */
-
 struct x13s_ec {
 	struct i2c_client *client;
 	struct regmap *regmap;
@@ -123,36 +305,69 @@ struct x13s_ec {
 	//int last_convertible_mode;
 };
 
+static int last_reg_state[0xff] = { 0 };
+static int x13s_ec_read(struct x13s_ec *ec, u8 reg, u8 *buf);
 static irqreturn_t x13s_ec_intr(int irq, void *data)
 {
 	struct x13s_ec *ec = data;
 	unsigned int event;
 
-	msleep(30);
-	regmap_read(ec->regmap, 0xf0, &event);
+	guard(mutex)(&ec->lock);
 
+	regmap_read(ec->regmap, 0xf0, &event);
 	if (event)
 		pr_err("got ev = 0x%x\n", event);
+
+	int i;
+	for (i = 0; i <= 0xff; i++) {
+		if (i == REG_QEV_X || i == REG_QEV_Y || i == 0xa8 || i == 0xa9 || i == 0xaa || i == 0xab || i == 0xac || i == 0xad || i == 0x8b)
+			continue;
+		u8 buf;
+		x13s_ec_read(ec, i, &buf);
+		if ((i == 0x77 && ((last_reg_state[i] ^ buf) == 0x04)))
+			continue;
+
+		if (buf != last_reg_state[i]) {
+			pr_err(" REG 0x%x changed from 0x%x to 0x%x!\n", i, last_reg_state[i], buf);
+			last_reg_state[i] =buf;
+		}
+	}
 
 	return IRQ_HANDLED;
 }
 
-static int x13s_ec_write(struct x13s_ec *ec, u16 reg, u8 val)
+static int x13s_ec_multi_write(struct x13s_ec *ec, u16 reg, u8 val)
 {
-	if (reg <= 0xff) {
-		return i2c_smbus_write_i2c_block_data(ec->client,
-						      REG_SMBUS_WRITE,
-						      2,
-						      (u8 []){ reg, val });
-	}
-
 	return i2c_smbus_write_i2c_block_data(ec->client,
 					      REG_SMBUS_MULTI_WRITE,
 					      3,
 					      (u8 []){ reg & 0xff, reg >> 8, val });
 }
 
-static int x13s_ec_read(struct x13s_ec *ec, u16 reg, u8 *buf)
+static int x13s_ec_write(struct x13s_ec *ec, u8 reg, u8 val)
+{
+	return i2c_smbus_write_i2c_block_data(ec->client,
+					      REG_SMBUS_WRITE,
+					      2,
+					      (u8 []){ reg, val });
+}
+
+static int x13s_ec_multi_read(struct x13s_ec *ec, u16 reg, u8 *buf)
+{
+	int ret;
+
+	ret = i2c_smbus_write_word_data(ec->client,
+					REG_SMBUS_MULTI_READ,
+					reg);
+	if (ret)
+		return ret;
+
+	return i2c_smbus_read_i2c_block_data(ec->client,
+					     REG_SMBUS_MULTI_READ,
+					     1, buf);
+}
+
+static int x13s_ec_read(struct x13s_ec *ec, u8 reg, u8 *buf)
 {
 	int ret;
 
@@ -161,18 +376,23 @@ static int x13s_ec_read(struct x13s_ec *ec, u16 reg, u8 *buf)
 	if (ret)
 		return ret;
 
-	return i2c_smbus_read_i2c_block_data(ec->client, 0x02, 1, buf);
+	return i2c_smbus_read_i2c_block_data(ec->client,
+					     REG_SMBUS_READ,
+					     1, buf);
 }
 
 static int x13s_ec_suspend(struct device *dev)
 {
 	struct x13s_ec *ec = dev_get_drvdata(dev);
+	int i;
 
 	if (ec->suspended)
 		return 0;
 
-	//regmap_write(ec->regmap, REG_SUS_CTL, REG_SUS_CTL_SUS_ENTER);
-	msleep(10); // 30?
+	for (i = 0; i < 3; i++) {
+		x13s_ec_write(ec, REG_SUS_CTL, REG_SUS_CTL_SUS_ENTER);
+		msleep(10);
+	}
 
 	gpiod_set_value(ec->reset_gpio, 1);
 
@@ -194,7 +414,7 @@ static int x13s_ec_resume(struct device *dev)
 
 	/* The EC or I2C host can be grumpy when waking up */
 	for (i = 0; i < 3; i++) {
-		//regmap_write(ec->regmap, REG_SUS_CTL, REG_SUS_CTL_SUS_EXIT);
+		x13s_ec_write(ec, REG_SUS_CTL, REG_SUS_CTL_SUS_EXIT);
 		msleep(10);
 	}
 
@@ -203,10 +423,16 @@ static int x13s_ec_resume(struct device *dev)
 	return 0;
 }
 
+// static int x13s_ec_get_temp(struct thermal_zone_device *tz, int *temp)
+// {
+	// TODO: check endianness :/
+	//return x13s_ec_multi_read(thermal_zone_device_priv(tz), temp)
+// }
+
 static const struct regmap_config regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 8,
-	.max_register = 0xff, // ??
+	.max_register = 0xff,
 };
 
 static int x13s_ec_probe(struct i2c_client *client)
@@ -248,27 +474,28 @@ static int x13s_ec_probe(struct i2c_client *client)
 	if (val)
 		dev_info(dev, "Found Lenovo EC v%u\n", val);
 
-	guard(mutex)(&ec->lock);
+	//guard(mutex)(&ec->lock);
 
 	msleep(100);
 	u8 buf[1] = {0};
-	ret = x13s_ec_read(ec, REG_CA, buf);
-	pr_err("buf[0xCA] = 0x%x\n", buf[0]);
 
-	msleep(100);
+	// devm_thermal_of_zone_register(bgp->dev, id, &data[id],&k3_of_thermal_ops);
 
-#if 0
+	gpiod_set_value(ec->reset_gpio, 0);
+	msleep(150);
+
+	u8 longbuf[1] = { 0 };
 	int i;
-	for (i = 0; i <= 0xff; i++) {
-		i2c_smbus_write_byte_data(client, 0x02, (u8)i);
-		i2c_smbus_read_i2c_block_data(client, 0x02, 1, buf);
-		pr_err("reg[%u] = 0x%x\n", i, buf[0]);
-		msleep(150);
+	for (i = 0; i < 6; i++) {
+		x13s_ec_multi_read(ec, 0x2020 + i, longbuf);
+		pr_err("TZ %u reports %u degC", i, longbuf[0]);
 	}
-#endif
 
-	ret = x13s_ec_write(ec, REG_KBD, REG_KBD_MICMUTE);
-	msleep(50);
+	x13s_ec_read(ec, 0x53, buf);
+	pr_err("EC [0x51] = 0x%x\n", buf[0]);
+	x13s_ec_write(ec, 0xcf, 0xff);
+	x13s_ec_read(ec, 0xcf, buf);
+	pr_err("EC [0xcf] = 0x%x\n", buf[0]);
 
 	return 0;
 }
